1ff93a12e6a40f2ccf51189fa76eaa21426dcc8b
ThuThuy237 <52254322+ThuThuy237@users.noreply.github.com>
2021-11-29 20:37:44 +0700

    update setting

--- jobReferralProject/settings.py
+++ jobReferralProject/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -199,7 +200,12 @@ DATETIME_FORMAT = '%m/%d/%Y %H:%M'
 STATIC_URL = '/static/'
 MEDIA_ROOT = '%s/jobreferralApp/' % BASE_DIR
 CKEDITOR_UPLOAD_PATH = 'uploads/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
