e17d931d6add01289cf53fbb452759d4fb2753f8
ThuThuy237 <52254322+ThuThuy237@users.noreply.github.com>
2021-11-29 20:40:41 +0700

    update

--- jobReferralProject/settings.py
+++ jobReferralProject/settings.py
@@ -202,10 +202,6 @@ MEDIA_ROOT = '%s/jobreferralApp/' % BASE_DIR
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
